b4c6c0b5c745c23da7777e91a05c1d1873f8d798
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-01-20 23:48:21 +0530

    Fix CreateUser Handler

--- handlers/createUser.go
+++ handlers/createUser.go
@@ -6,33 +6,46 @@ import (
 	"net/http"
 
 	"github.com/deepanshumishraa/models"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 func CreateUserHandler(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var user models.User
-		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		var req struct {
+			Name string `json:"name"`
+		}
+
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 			http.Error(w, "Invalid request body", http.StatusBadRequest)
 			return
 		}
 
-		if user.Name == "" || user.APIKEY == "" {
-			http.Error(w, "Name and APIKEY are required", http.StatusBadRequest)
+		if req.Name == "" {
+			http.Error(w, "Name is required", http.StatusBadRequest)
 			return
 		}
 
-		result := db.Create(&user)
-		if result.Error != nil {
-			log.Printf("Error creating user: %v", result.Error)
+		apiKey := uuid.NewString()
+
+		user := models.User{
+			Name:   req.Name,
+			APIKEY: apiKey,
+		}
+
+		if err := db.Create(&user).Error; err != nil {
+			log.Printf("Error creating user: %v", err)
 			http.Error(w, "Failed to create user", http.StatusInternalServerError)
 			return
 		}
+
 
 		log.Printf("User created: %+v", user)
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(user)
+		json.NewEncoder(w).Encode(map[string]string{
+			"apiKey": user.APIKEY,
+		})
 	}
 }
