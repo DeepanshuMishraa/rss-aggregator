6764d6fdc033bfa65b1119dafff9dcd6c3bf110b
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-01-20 19:41:08 +0530

    server setup

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
-package main;
-
+package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 	"os"
-);
 
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
+func main() {
 
-func main(){
-	fmt.Println("hello world");
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT is not set")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server is running on port %v", port)
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
