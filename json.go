3018e62ade9cde589c74f493c1023c75b31b0aec
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-01-20 20:03:15 +0530

    BoilerPlate for server

--- json.go
+++ json.go
@@ -6,6 +6,20 @@ import (
 	"net/http"
 )
 
+func RespondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5XX Error", msg)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	RespondWithJson(w, code, errResponse{
+		Error: msg,
+	})
+}
+
 func RespondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.Marshal(payload)
 
