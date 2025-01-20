e9ecb31dddf3a20b4bae20be538dff982c4e66b3
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-01-20 20:24:40 +0530

    gorm init

---
+++ db.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=admin port=5432 dbname=postgres sslmode=disable"
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
