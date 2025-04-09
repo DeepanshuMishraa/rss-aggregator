309464637bb84449190187b785af9fee4bfe72f9
Deepanshu Mishra <d4deepanshu723@gmail.com>
2025-04-09 19:30:48 +0530

    Setup router

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	fmt.Println("Port: ", port)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:300
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server starting on port %s\n", port)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
+
 	fmt.Println("Hello, World!")
 }
